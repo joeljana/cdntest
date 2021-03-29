c7b5982a9dac021d29ccd4bf35d056dfcb83be54
joeljana <79537864+joeljana@users.noreply.github.com>
2021-03-29 17:49:45 +0530

    Update watch.js

--- watch.js
+++ watch.js
@@ -9,7 +9,7 @@ var text = document.querySelector ("#nowtext") ;
 // function start here 
 function abpananda(){
     pimg.src='https://i.ibb.co/0ntcrRH/abp-ananda.png';
-    text.innerHTML = ("ABP Ananda new cdn") ;
+    text.innerHTML = ("ABP Ananda new cdn 25") ;
 }
 function news18bangla(){
     pimg.src = "https://i.ibb.co/YRNvJ2R/news18-bangla.png" ;
